4c6f7e3c2d36ba45f835bf95051957d0eb4c37ea
Abhishek Sunkale <91458513+abhishek6969@users.noreply.github.com>
2026-02-26 11:24:50 +0530

    Update var.tf

--- vms-terraform-deploy/var.tf
+++ vms-terraform-deploy/var.tf
@@ -6,6 +6,7 @@ variable "vms" {
     publisher = string
     version = string
     plan_required = bool
+    data_disk_enabled = bool
   }))
   default = {
     "Ubuntu-vm" = {
@@ -15,6 +16,7 @@ variable "vms" {
       publisher = "Canonical"
       version = "latest"
       plan_required = false # This VM does not require a plan
+      data_disk_enabled = true # Enable data disk for this VM
     },
     rhel-vm = {
       name  = "rhel-vm"
@@ -23,6 +25,7 @@ variable "vms" {
       sku       = "rocky-linux-9-lvm" # Or "8-base" for Rocky 8
       version   = "latest"
       plan_required = true # This VM requires a plan
+      data_disk_enabled = true # No data disk for this VM
     },
     "suse-vm" = {
       name  = "suse-vm"
@@ -31,6 +34,7 @@ variable "vms" {
       sku       = "gen2"
       version   = "latest"
       plan_required = false # This VM does not require a plan
+      data_disk_enabled = true # No data disk for this VM
     }
   }
 }
